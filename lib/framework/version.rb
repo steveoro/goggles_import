5e95ba3545326c7e0e03309a89984be9122f5b36
steveoro <steve.alloro@gmail.com>
2019-11-03 22:48:22 +0100

    re-organized crawled results according to season ID; re-synch w/ updated core

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '6'
 
   # Minor version.
-  MINOR   = '404'
+  MINOR   = '421'
 
   # Current build version.
-  BUILD   = '20190617'
+  BUILD   = '20191103'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
